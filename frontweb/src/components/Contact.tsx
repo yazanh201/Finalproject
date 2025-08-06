ce2431a151138cad589e71ec887950fadf190899
mahdir373 <romhe492@gmail.com>
2025-08-06 23:21:56 +0300

    fixing

--- frontweb/src/components/Contact.tsx
+++ frontweb/src/components/Contact.tsx
@@ -25,7 +25,7 @@ const Contact = () => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/inquiries", {
+      const res = await fetch("https://garage-backend-z20t.onrender.com/api/inquiries", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
